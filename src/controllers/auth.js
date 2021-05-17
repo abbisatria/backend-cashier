9c0f4c0e6b2b621d26292f31a5346ddf42eb1222
abbisatria <abbisatria48@gmail.com>
2021-05-17 11:38:06 +0700

    create endpoint forgot password and reset password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -67,3 +67,45 @@ exports.verificationEmail = async (req, res) => {
     return response(res, 400, false, 'Bad Request')
   }
 }
+
+exports.forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body
+    const isExists = await userModel.getUserByCondition({ email })
+    if (isExists.length > 0) {
+      const token = jwt.sign({ id: isExists[0].id }, APP_KEY)
+      sendEmail(isExists[0].id, `${APP_URL}auth/verificationForgotPassword/${token}`, 'Reset Password', 'To reset your password, click the following link and follow the instructions.')
+      return response(res, 200, true, 'Please Check Email to Reset Password')
+    } else {
+      return response(res, 400, false, 'Email Not Registered')
+    }
+  } catch (e) {
+    return response(res, 400, false, 'Bad Request')
+  }
+}
+
+exports.verificationForgotPassword = async (req, res) => {
+  try {
+    const { token } = req.params
+    return res.redirect(`cashier://forgotPassword/${token}`)
+  } catch (e) {
+    return response(res, 400, false, 'Bad Request')
+  }
+}
+
+exports.resetPassword = async (req, res) => {
+  try {
+    const { token } = req.params
+    const { password } = req.body
+    const data = jwt.verify(token, APP_KEY)
+    const salt = await bcrypt.genSalt()
+    const encryptedPassword = await bcrypt.hash(password, salt)
+    const update = await userModel.updateUser(data.id, { password: encryptedPassword })
+    if (update.affectedRows > 0) {
+      return response(res, 200, true, 'Reset Password Success')
+    }
+    return response(res, 400, false, 'Failed Reset Password')
+  } catch (e) {
+    return response(res, 400, false, 'Bad Request')
+  }
+}
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,6 +3,9 @@ const authController = require('../controllers/auth')
 
 routes.post('/register', authController.register)
 routes.post('/login', authController.login)
+routes.post('/forgotPassword', authController.forgotPassword)
+routes.patch('/resetPassword/:token', authController.resetPassword)
 routes.get('/verification/:id', authController.verificationEmail)
+routes.get('/verificationForgotPassword/:token', authController.verificationForgotPassword)
 
 module.exports = routes
